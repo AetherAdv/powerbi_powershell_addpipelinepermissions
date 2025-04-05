89c9318b3480092f9615fb52eb85f2c9365a8979
James Mounsey-Moran <james@jamesmm.com>
2025-04-05 10:19:01 +0100

    Add files via upload

---
+++ powerbi_powershell_adduserspipelines.ps1
@@ -0,0 +1,26 @@
+﻿Connect-PowerBIServiceAccount
+
+
+# Get list of pipelines (optional)
+$apiUrlPipelines = "admin/pipelines"
+$pipelineList = Invoke-PowerBIRestMethod -Url $apiUrlPipelines -Method Get
+
+#---------------------------------
+
+$pipelineId = "####"
+$userEmail = "user@example.com"
+
+# Get users assigned to a pipeline
+$apiUrlUsers = "admin/pipelines/$pipelineId/users"
+$pipelineUsers = Invoke-PowerBIRestMethod -Url $apiUrlUsers -Method Get
+$pipelineUsers | ConvertTo-Json -Depth 2
+
+
+# Add a user as Admin
+$body = @{
+    identifier = $userEmail
+    accessRight = "Admin"
+    principalType = "User"
+} | ConvertTo-Json -Depth 3
+
+Invoke-PowerBIRestMethod -Url $apiUrlUsers -Method Post -Body $body -ContentType "application/json"
